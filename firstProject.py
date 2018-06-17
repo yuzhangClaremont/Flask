713a5adc262e4135d0474db855687a7c171772f8
Yun <yuzhang@g.hmc.edu>
2018-06-18 00:52:54 +0800

    bootstrap

--- firstProject.py
+++ firstProject.py
@@ -1,10 +1,31 @@
 
-from flask import Flask, request
+from flask import Flask, render_template , request
+# import 
 app = Flask(__name__) # mudule name variable
 
+# user post is a list of dictionaries, each is a single post
+posts = [
+    {
+        'author': 'Corey S',
+        'title': 'blog',
+        'content': 'fff'
+    },
+        {
+        'author': 'zey S',
+        'title': 'alog',
+        'content': 'conccc'
+    }
+]
+ 
 @app.route("/")# when go to home page of website, run html
-def index():
-    return "<h2>method used %s</h2>" % request.method #GET
+#or use two route on same function
+@app.route("/home/<name>") # page name must correspond function name
+# passed in name
+@app.route("/home/")# none by default
+def home(name=None):
+    return render_template('home.html',p=posts, n = name)
+    # posts is global variable defined above, and in html template be named as p
+    # return "<h2>method used %s</h2>" % request.method #GET
 
 @app.route("/samon", methods=['GET','POST'])# when go to http://localhost:5000/samon, run html
 # METHOD able to handle get and post request method
@@ -15,11 +36,19 @@ def samon():
     else:
         return "you are using GET"
 
+@app.route("/about")
+def about():
+    return render_template('about.html',title='About')
 
-@app.route("/profile/<username>")# extrct username and return in html
-#http://localhost:5000/profile/yun
-def profile(username):
-    return "<p>hey %s</p>" %username
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+# @app.route("/profile/<name>")# extrct username and return in html
+# #http://localhost:5000/profile/yun
+# def profile(name):
+#     # return "<p>hey %s</p>" %username
+#     return render_template("profile.html")
+
+# NOT NEEDED WITH flask run COMMAND
+if __name__ == '__main__':
+    # create server and wait for response
+    app.run(debug=True)# port =8000
